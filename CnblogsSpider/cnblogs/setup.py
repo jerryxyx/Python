3db1244697cd16e254d71179327dacb2822aa2bc
wentao <wentao@yiqixie.com>
2017-08-09 00:57:28 -0400

    rearrangement

--- CnblogsSpider/cnblogs/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='scrapy-mymodule',
-  entry_points={
-    'scrapy.commands': [
-      'crawlall=cnblogs.commands:crawlall',
-    ],
-  },
- )
