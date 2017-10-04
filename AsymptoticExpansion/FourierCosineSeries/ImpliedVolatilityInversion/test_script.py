64021189d37275d880808cceeefd28147d984f54
wentao <wentao@yiqixie.com>
2017-10-04 12:38:18 -0400

    mac

--- AsymptoticExpansion/FourierCosineSeries/ImpliedVolatilityInversion/test_script.py
+++ AsymptoticExpansion/FourierCosineSeries/ImpliedVolatilityInversion/test_script.py
@@ -12,8 +12,8 @@ r = 0.01
 q = 0
 sigmaBSM = 0.25
 # numStrikes = 10
-numGrid = 2**6
-truncatedOrder = 10 #that is we want to obtain (convergent rapidly)
+numGrid = 2**6  # 64should be a proper number
+truncatedOrder = 10 # that is we want to obtain (convergent rapidly)
 #############################
 # Derivative parameters
 (a,b) = preprocessing.generateTruncatedInterval(S0,strike,T,r,q,sigmaBSM,"BSM")
@@ -21,13 +21,15 @@ print(a,b)
 m = preprocessing.calculateConstantTerm(S0,strike,T,r,q,a)
 #############################
 
-Vk = Vk_utils.calculateVkPut(strike,a,b,numGrid)
+Vk = Vk_utils.calculateVkPut(strike,a,b,numGrid=5)
+print(np.shape(Vk))
 print("Vk",Vk)
-ckList = [k*np.pi/(b-a) for k in range(20)]
+ckList = [k*np.pi/(b-a) for k in range(5)]
 # Truncated order should satisfies j >> (k/2)^2. j = 5*j^2 is a proper value
 # Dl3 = Dlk_utils.calculateHybridSeries(5*np.pi/(b-a),m,truncatedOrder=45)
-Dl5 = Dlk_utils.calculateHybridSeries(5*np.pi/(b-a),m,truncatedOrder=100)
-print("k=5,Dlk",Dl5)
+# Dl5 = Dlk_utils.calculateHybridSeries(5*np.pi/(b-a),m,truncatedOrder=100)
+# print("k=5,Dlk",Dl5)
 # Dl10 = Dlk_utils.calculateHybridSeries(10*np.pi/(b-a),m,truncatedOrder=100)# here should be 550 to see convergence
-# DlkList = [Dlk_utils.calculateHybridSeries(ck,m,truncatedOrder=10) for ck in ckList]
-# print("k=0..19,Dlk",DlkList)
+DlkList = [Dlk_utils.calculateHybridSeries(ck,m,truncatedOrder=64) for ck in ckList] # truncationOrder should be 64^2
+print("k=0..5,Dlk",DlkList)
+print(np.shape(DlkList))
