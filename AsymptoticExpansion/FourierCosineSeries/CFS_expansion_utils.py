355412ccd3409e87fe43f40e1b3ef29732313680
jerryxyx <jerryxyx@163.com>
2017-10-11 13:49:20 -0400

    oct11

--- AsymptoticExpansion/FourierCosineSeries/CFS_expansion_utils.py
+++ AsymptoticExpansion/FourierCosineSeries/CFS_expansion_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import preprocessing
 import time
+from math import factorial,log
 
 def calculate_Vk_put_CFS(a,b,N1,strike):
     k_array = np.linspace(0,N1-1,N1)
@@ -29,3 +30,24 @@ def putOptionPriceCFS(S0,strike,T,r,q,sigma,quantile,numGrid,showDuration=False)
     if(showDuration==True):
         print("consuming time for call option using CFS:", tack - tick)
     return putOptionPrice
+
+def calculateIVCoefficientArray(S0,strike,T,r,q,sigmaBSM,N1,N2,quantile):
+    (a,b) = preprocessing.calculateToleranceInterval(S0,strike,T,r,q,sigmaBSM,quantile)
+    m = (r-q)*T + np.log(S0/strike)
+    ckArray = np.array([k*2*np.pi/(b-a) for k in range(N1)])
+    complexCkArray = np.array([k*2.j*np.pi/(b-a) for k in range(N1)])
+    VkArray = calculate_Vk_put_CFS(a,b,N1,strike)
+    coeffArray = np.zeros(N2,dtype=np.complex64)
+    for l in range(N2):
+        # coeff1 = np.exp(-complex(0,ckArray*m))
+        coeff1 = np.exp(-complexCkArray*m)
+        print("coeff1[",l,"]",coeff1)
+        coeff2 = ((complexCkArray+complexCkArray**2)/2)**l /factorial(l)
+        print("coeff2[", l, "]", coeff2)
+        coeffArray[l] = np.sum(coeff1*coeff2*VkArray)
+        # coeff2 = np.array([(complex(-ck**2,ck)/2)**l / factorial(l) for ck in ckArray])
+        # coeffArray[l] = np.sum(coeff1*coeff2*VkArray)
+        # coeffArray[l] = np.sum([complex(-.5*ck**2,.5*ck)**l/factorial(l)*np.exp(complex(0,-m*ck))*Vk for ck,Vk in zip(ckArray,VkArray)])
+
+    print("coffs",coeffArray)
+    return coeffArray.real
